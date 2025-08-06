c884c14c360c4376f4804317b8687ede9dc2f033
Esteban Valencia <esteval_15@hotmail.com>
2025-08-06 13:53:07 -0700

    Resuelto problema de compilacion de dagger hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlinAndroidKsp)
+    id("kotlin-kapt")
     alias(libs.plugins.hiltAndroid)
 }
 
